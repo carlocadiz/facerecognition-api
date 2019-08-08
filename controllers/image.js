13689be8ad0b698d1c1b8f64b0dcd572a1bec402
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-08 09:31:26 -0400

    fixed validation and security issues

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '8cebe6e687e843c5a0338128940e53b7'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with api'))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +25,6 @@ const handleImage = (req, res, db) => {
   }
 
   module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
   }
--- controllers/register.js
+++ controllers/register.js
@@ -2,6 +2,9 @@
 
 const handleRegister = (req, res, db, bcrypt, saltRounds) => {
   const {email, name, password} = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
   const salt = bcrypt.genSaltSync(saltRounds);
   const hash = bcrypt.hashSync(password, salt);
 
